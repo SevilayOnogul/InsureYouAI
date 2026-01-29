34a62f1c6ba4f35c0c363a374b03c1127f124b0e
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-01-29 16:16:32 +0300

    Add OpenAI integration

--- InsureYouAI/Controllers/ArticleController.cs
+++ InsureYouAI/Controllers/ArticleController.cs
@@ -1,6 +1,7 @@
 ﻿using InsureYouAI.Context;
 using InsureYouAI.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 
 namespace InsureYouAI.Controllers
 {
@@ -58,6 +59,58 @@ namespace InsureYouAI.Controllers
             _context.SaveChanges();
             return RedirectToAction("ArticleList");
 
+        }
+        [HttpGet]
+        public IActionResult CreateArticleWithOpenAI()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>CreateArticleWithOpenAI(string prompt)
+        {
+            var apiKey = "API_KEY";
+
+            using var client= new HttpClient();
+            client.DefaultRequestHeaders.Authorization=new AuthenticationHeaderValue("Bearer",apiKey);
+
+            var requestData = new
+            {
+                model="gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new{role="System",content="Sen bir sigorta şirketi için çalışan, içerik yazarlığı yapan bir yapay zekasın. " +
+                    "Kullanıcının verdiği özet ve anahtar kelimelere göre, sigortacılık sektörüyle ilgili makale üret. En az 3000 karakter olsun."},
+                    new{role="user",content=prompt } 
+                },
+                temperature=0.7
+            };
+
+            var response = await client.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestData);
+            if(response.IsSuccessStatusCode)
+            {
+                var result=await response.Content.ReadFromJsonAsync<OpenAIResponse>();
+                var content = result.choices[0].message.content;
+                ViewBag.article=content;
+            }
+            else
+            {
+                ViewBag.article = "Bir hata oluştu:" + response.StatusCode;
+            }
+            return View();
+        }
+        public class OpenAIResponse
+        {
+            public List<Choice> choices { get; set; }
+        }
+        public class Choice
+        {
+            public Message message { get; set; } 
+        }
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
         }
     }
 
