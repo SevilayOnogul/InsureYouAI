b2f94eda3a80e0db6c394c6d40702a708863b5e9
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-02-04 02:32:06 +0300

    feat: dashboard updates

---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DashboardSubCharts1ComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using InsureYouAI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardSubCharts1ComponentPartial:ViewComponent
+    {
+        private readonly InsureContext _context;
+
+        public _DashboardSubCharts1ComponentPartial(InsureContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var policyData=_context.Policies
+                .GroupBy(p=>p.PolicyType)
+                .Select(g => new
+                {
+                    PolicyType=g.Key,
+                    Count=g.Count(),
+                }).ToList();
+
+            ViewBag.policyData=JsonConvert.SerializeObject(policyData);
+
+            return View();
+        }
+    }
+}
---
+++ InsureYouAI/ViewComponents/DashboardViewComponents/_DashboardSubChartsComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using InsureYouAI.Context;
+using InsureYouAI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsureYouAI.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardSubChartsComponentPartial:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+     
+            return View();
+        }
+    }
+}
